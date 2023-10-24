89dc7b3a1e08402dad753c75237ae1cc767c4278
M4niaDaniel <148873201+M4niaDaniel@users.noreply.github.com>
2023-10-24 14:58:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,31 @@ using namespace std;
 
 string name;
 int i = 3;
+int anwsers;
 
-void question_request(string question, string ans_a, string ans_b, string ans_c){
-
+void question_request(string question, string ans_a, string ans_b, string ans_c, string ans){
+    system("CLS");
+    cout << question << " ?" << endl;
+    cout << "a) " << ans_a << endl;
+    cout << "b) " << ans_b << endl;
+    cout << "c) " << ans_c << endl;
+    cout << "Twoja Odpowiedź(a,b,c): ";
+    string anwser;
+    getline(cin, anwser);
+    if(anwser == ans){
+        cout << "Dobra odpowiedź" << endl;
+        anwsers += 1;
+    }
+    else{
+        cout << "Zła odpowiedź" << endl;
+    }
+    system("CLS");
 }
 
 int main(void){
+    setlocale(LC_CTYPE, "Polish");
     cout << "Podaj Imię: ";
-    cin >> name;
+    getline(cin, name);
     system("CLS");
     do{
         cout << i << "...";
@@ -20,4 +37,7 @@ int main(void){
         Sleep(1000);
     }
     while(i > 0);
+    question_request("Ile rąk ma człowiek", "1", "2", "3", "b");
+    cout << "Ilość poprawnych odpowiedzi: " << anwsers;
+    return 0;
 }
