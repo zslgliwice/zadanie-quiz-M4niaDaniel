513d5bd3256cf3dd7dcc908e4112647b90dcd594
M4niaDaniel <148873201+M4niaDaniel@users.noreply.github.com>
2023-10-24 14:32:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,23 @@
 #include <iostream>
 #include <windows.h>
-#include <cstdlib>
 
 using namespace std;
 
-string imie;
-string odp;
+string name;
+int i = 3;
 
-int main()
-{
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
+void question_request(string question, string ans_a, string ans_b, string ans_c){
 
-    system("cls");
-    cout<< "3";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
+}
 
-    cout << imie << " witaj w tescie z programowania\n";
-
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
+int main(void){
+    cout << "Podaj Imię: ";
+    cin >> name;
+    system("CLS");
+    do{
+        cout << i << "...";
+        i--;
+        Sleep(1000);
     }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
-
-    }
-    Sleep(3000);
-    system("cls");
-
-
-
-    return 0;
+    while(i > 0);
 }
